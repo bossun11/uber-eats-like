4836b691b8b0b43fde21641de7101789bcd7c2cc
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-14 17:04:12 +0900

    Add: フード一覧ページにAPIから取得したデータを表示

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import { useEffect, useReducer } from "react";
 import { useParams } from "react-router-dom";
+import { fetchFoods } from "../apis/foods";
+import {
+  initialState as foodsInitialState,
+  foodsActionTypes,
+  foodsReducer,
+} from "../reducers/foods";
+import { REQUEST_STATE } from "../constants";
 
 const Foods = () => {
   const { restaurantsId } = useParams();
+  const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
+
+  useEffect(() => {
+    dispatch({ type: foodsActionTypes.FETCHING });
+    fetchFoods(restaurantsId).then((data) => {
+      dispatch({
+        type: foodsActionTypes.FETCH_SUCCESS,
+        payload: {
+          foods: data.foods,
+        },
+      });
+    });
+  }, []);
   return (
     <>
-      Foods
-      <p>restaurantsIdは {restaurantsId} です</p>
+      {foodsState.fetchState === REQUEST_STATE.LOADING ? (
+        <p>ロード中...</p>
+      ) : (
+        foodsState.foodsList.map((food) => <div key={food.id}>{food.name}</div>)
+      )}
     </>
   );
 };
