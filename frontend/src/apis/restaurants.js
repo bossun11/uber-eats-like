0ce350fd8eabcc382988e1065b93ee23642dca4e
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-14 10:45:27 +0900

    Add: レストラン一覧のAPIを取得する関数を定義

---
+++ frontend/src/apis/restaurants.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { restaurantsIndex } from "../urls/index";
+
+const fetchRestaurants = () => {
+  return axios
+    .get(restaurantsIndex)
+    .then((res) => {
+      return res.data;
+    })
+    .catch((e) => console.error(e));
+};
