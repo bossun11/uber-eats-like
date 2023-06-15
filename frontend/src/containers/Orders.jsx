5064c92e01a1318f4cbadf4e22403f907f3b47e1
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-15 20:32:42 +0900

    Add: 注文を登録する関数を作成

--- frontend/src/containers/Orders.jsx
+++ frontend/src/containers/Orders.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useReducer } from "react";
 import { fetchLineFoods } from "../apis/line_foods";
+import { postOrder } from "../apis/orders";
 
 // reducer
 import {
@@ -24,6 +25,16 @@ const Orders = () => {
       })
       .catch((e) => console.error(e));
   }, []);
+
+  const postLineFoods = () => {
+    dispatch({ type: lineFoodsActionTypes.POSTING });
+    postOrder({
+      line_food_ids: state.lineFoodsSummary.line_food_ids,
+    }).then(() => {
+      dispatch({ type: lineFoodsActionTypes.POST_SUCCESS });
+      window.location.reload();
+    });
+  };
 
   return <div>Orders</div>;
 };
