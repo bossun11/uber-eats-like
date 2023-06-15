2134b7c8273af739bf65f813d0ebf1352d4d2ffd
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-15 21:45:01 +0900

    Add: 注文画面を作成

--- frontend/src/containers/Orders.jsx
+++ frontend/src/containers/Orders.jsx
@@ -1,6 +1,13 @@
 import { useEffect, useReducer } from "react";
 import { fetchLineFoods } from "../apis/line_foods";
 import { postOrder } from "../apis/orders";
+import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
+
+// components
+import OrderDetailItem from "../components/OrderDetailItem";
+import OrderButton from "../components/Buttons/OrderButton";
+import { CircularProgress } from "@mui/material";
 
 // reducer
 import {
@@ -8,6 +15,31 @@ import {
   lineFoodsActionTypes,
   lineFoodsReducer,
 } from "../reducers/lineFoods";
+
+// images
+import MainLogo from "../images/logo.png";
+
+// constants
+import { REQUEST_STATE } from "../constants";
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`;
+
+const OrderListWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const OrderItemWrapper = styled.div`
+  margin-bottom: 50px;
+`;
 
 const Orders = () => {
   const [state, dispatch] = useReducer(lineFoodsReducer, initialState);
@@ -36,7 +68,63 @@ const Orders = () => {
     });
   };
 
-  return <div>Orders</div>;
+  const orderButtonLabel = () => {
+    switch (state.postState) {
+      case REQUEST_STATE.LOADING:
+        return "注文中...";
+      case REQUEST_STATE.OK:
+        return "注文が完了しました！";
+      default:
+        return "注文を確定する";
+    }
+  };
+
+  return (
+    <>
+      <HeaderWrapper>
+        <Link to="/restaurants">
+          <MainLogoImage src={MainLogo} alt="main logo" />
+        </Link>
+      </HeaderWrapper>
+      <OrderListWrapper>
+        <div>
+          <OrderItemWrapper>
+            {/* APIローディング中はくるくる回るローディングコンポーネントを表示 */}
+            {state.fetchState === REQUEST_STATE.LOADING ? (
+              <CircularProgress />
+            ) : (
+              state.lineFoodsSummary && (
+                <OrderDetailItem
+                  restaurantFee={state.lineFoodsSummary.restaurant.fee}
+                  restaurantName={state.lineFoodsSummary.restaurant.name}
+                  restaurantId={state.lineFoodsSummary.restaurant.id}
+                  timeRequired={state.lineFoodsSummary.restaurant.time_required}
+                  foodCount={state.lineFoodsSummary.count}
+                  price={state.lineFoodsSummary.amount}
+                />
+              )
+            )}
+          </OrderItemWrapper>
+          <div>
+            {state.fetchState === REQUEST_STATE.OK &&
+              state.lineFoodsSummary && (
+                <OrderButton
+                  onClick={() => postLineFoods()}
+                  disabled={
+                    state.postState === REQUEST_STATE.LOADING ||
+                    state.postState === REQUEST_STATE.OK
+                  }
+                >
+                  {orderButtonLabel()}
+                </OrderButton>
+              )}
+            {state.fetchState === REQUEST_STATE.OK &&
+              !state.lineFoodsSummary && <p>注文予定の商品はありません</p>}
+          </div>
+        </div>
+      </OrderListWrapper>
+    </>
+  );
 };
 
 export default Orders;
