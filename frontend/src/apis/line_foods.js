3dad7c3a45df2f7f0767a14e767a15dd67d6e9b9
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-15 14:14:40 +0900

    Add: 仮注文を置き換えて新しい仮注文を作成するためのAPI関数を作成

--- frontend/src/apis/line_foods.js
+++ frontend/src/apis/line_foods.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { lineFoods } from "../urls/index";
+import { lineFoods, lineFoodsReplace } from "../urls/index";
 
 const postLineFoods = (params) => {
   return axios
@@ -14,3 +14,19 @@ const postLineFoods = (params) => {
       throw e;
     });
 };
+
+const replaceLineFoods = (params) => {
+  return axios
+    .put(lineFoodsReplace, {
+      food_id: params.foodId,
+      count: params.count,
+    })
+    .then((res) => {
+      return res.data;
+    })
+    .catch((e) => {
+      throw e;
+    });
+};
+
+export { postLineFoods, replaceLineFoods };
