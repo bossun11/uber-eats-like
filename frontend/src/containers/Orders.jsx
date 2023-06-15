b81ac096fe9f83228a0bcbdc821eb9dcc859e0ae
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-15 20:05:32 +0900

    Add: lineFoodsReducerを作成

--- frontend/src/containers/Orders.jsx
+++ frontend/src/containers/Orders.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import { useEffect, useReducer } from "react";
+import { fetchLineFoods } from "../apis/line_foods";
+
+// reducer
+import {
+  initialState,
+  lineFoodsActionTypes,
+  lineFoodsReducer,
+} from "../reducers/lineFoods";
 
 const Orders = () => {
+  const [state, dispatch] = useReducer(lineFoodsReducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: lineFoodsActionTypes.FETCHING });
+    fetchLineFoods()
+      .then((data) => {
+        dispatch({
+          type: lineFoodsActionTypes.FETCH_SUCCESS,
+          payload: {
+            lineFoodsSummary: data,
+          },
+        });
+      })
+      .catch((e) => console.error(e));
+  }, []);
+
   return <div>Orders</div>;
 };
 
